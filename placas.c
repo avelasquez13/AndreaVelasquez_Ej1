06719eea603f82a3a6ac5040023e6a71a154901a
Felipe Velasquez <f.velasquez10@uniandes.edu.co>
2017-02-12 13:40:43 -0500

    debug send/recv2

--- placas.c
+++ placas.c
@@ -16,7 +16,7 @@ int main(){
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	//TODO arreglar l/h y N
-  float L = 5, l = 2, d = 1, h = 5.0/32, V0 = 100, N = 200*pow((L/h), 2);
+  float L = 5, l = 2, d = 1, h = 5.0/32, V0 = 100, N = 2*pow((L/h), 2);
   int n = 32;
   
   //inicializa la matriz
