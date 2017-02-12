1b4bb110ac175544f9337d33db5529946540c873
Andrea Velasquez <a.velasquez12@uniandes.edu.co>
2017-02-12 17:16:42 -0500

    arregla grafica

--- grafica.py
+++ grafica.py
@@ -1,36 +1,21 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
+import matplotlib.pyplot as plt
 
-f = open('potencial.dat', 'r')
-datoslist = f.readlines()
-f.close()
+datosnp = np.loadtxt('potencial.dat')
+datos = np.array_split(datosnp, 3) 
 
-datosnp = np.array(datoslist)
-datos = np.array_split(datosnp, 3)
-
-potencial = np.array_split(datos[0], 250)
-Ex = np.array_split(datos[1], 250)
-Ey = np.array_split(datos[2], 250)
-
-for i in range(len(potencial)):
-    potencial[i] = np.asfarray(potencial[i])
-
-for i in range(len(Ex)):
-    Ex[i] = np.asfarray(Ex[i])
-
-for i in range(len(Ey)):
-    Ey[i] = np.asfarray(Ey[i])
+potencial = datos[0]
+Ex = datos[1]
+Ey = datos[2]
 
 
-plt.imshow(np.array(potencial).T, extent = [0, 5, 0, 5])
+plt.imshow(potencial, extent = [0, 5, 0, 5])
 
 x = np.linspace(0, 5, 250)
 y = np.linspace(5, 0, 250)
-plt.streamplot(x, y, np.array(Ex).T, np.array(Ey).T, color = 'k', density = 1.3)
+plt.streamplot(x, y, Ex, Ey, color = 'k', density = 1.3)
 
 plt.ylim([0, 5])
 
 plt.title('Potencial y campo de placas paralelas')
 plt.savefig('placas.pdf')
-
